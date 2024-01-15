defa637668de64408a21431f5f29f54f4c2b4f8e
RifaldyAldy <112386802+RifaldyAldy@users.noreply.github.com>
2024-01-15 08:41:38 +0700

    Update index.js

--- index.js
+++ index.js
@@ -26,6 +26,10 @@ app.use("/uploads", express.static("uploads"));
 require("./app/routes/routes")(app);
 const port = process.env.port || 3000;
 
+app.get('/', (req,res) => {
+  res.send('ok')
+}
+
 app.listen(port, () => {
   console.log(`Server berjalan di http:${port}/product`);
 });
